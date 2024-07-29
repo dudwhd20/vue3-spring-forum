2762fdc5db9192592bac1976010cdde3a0da25ba
soyj <dudwhd20@naver.com>
2024-07-29 20:02:22 +0900

    feat: Quasar 프레임워크 추가 및  기본 설정 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,9 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/src/css/index.sass'
+import Quasar from 'quasar'
 
 const app = createApp(App)
-
+app.use(Quasar,{
+  plugins:{}
+})
 app.use(createPinia())
 app.use(router)
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {transformAssetUrls}
+    }),
     vueJsx(),
     vueDevTools(),
+    quasar({
+      sassVariables: 'src/quasar.variables.sass'
+    })
   ],
   resolve: {
     alias: {
